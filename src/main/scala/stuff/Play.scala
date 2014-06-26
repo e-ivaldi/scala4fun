87d29c28536b823819b6e7ed74635176b998e372
Emanuele Ivaldi <emanuele.ivaldi@gmail.com>
2014-06-26 23:14:26 +0100

    p02 and p03 added

--- src/main/scala/stuff/Play.scala
+++ src/main/scala/stuff/Play.scala
@@ -19,4 +19,41 @@ object Play extends App {
   println("last element:" + findLastElement(list))
   println("last element (pm):" + findLastElementPatternMatching(list))
 
+  //P02 (*) Find the last but one element of a list.
+
+  // tip:
+  // list.init.last -> init get all the elements except the last
+  def findPenultimateElement[T](list: List[T]): T =
+    if (list.size < 2) throw new NoSuchElementException
+    else
+      list(list.length - 2)
+
+  def findPenultimateElementPatternMatching[T](list: List[T]): T = list match {
+    case e1 :: e2 :: Nil => e1
+    case e :: tail => findPenultimateElementPatternMatching(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  println("penultimate element:" + findPenultimateElement(list))
+  println("penultimate element (pm):" + findPenultimateElementPatternMatching(list))
+
+  //P03 (*) Find the Kth element of a list.
+
+  def findKthElement[T](list: List[T], k: Int): T = {
+    def isInputValid() : Boolean = k > -1 && k < list.size
+    if(isInputValid()) list(k) else
+      throw new NoSuchElementException
+  }  
+
+  def findKthElementPatternMatching[T](list: List[T], k: Int): T = (list, k) match {
+    case (head :: tail, 0) => head
+    case (head :: tail, _) => findKthElementPatternMatching(tail, k - 1)
+    case (Nil, _ ) => throw new NoSuchElementException
+  }
+
+  println("Kth element:" + findKthElement(list, 0))
+  println("Kth element:" + findKthElement(list, 3))
+  println("Kth element (pm):" + findKthElementPatternMatching(list, 0))
+  println("Kth element (pm):" + findKthElementPatternMatching(list, 3))
+
 }
